7684870296f0b47e3cf110588294f31473aeb991
rydvone <rydvone@gmail.com>
2022-07-15 01:31:53 +0300

    refactor: add search focus

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,8 +1,16 @@
 import './style.css';
-import initRangeWeight from './ts/components/filters/range/rangeWeight';
+import initRangeWeight from './ts/components/filters/range/filterRangeFunc';
 
 window.onload = () => {
   console.log('Hello, friend.');
+
+  function setFocus() {
+    const searchIn = document.querySelector('.search') as HTMLInputElement;
+    console.log(searchIn);
+    searchIn.focus();
+  }
+  setFocus();
+
   initRangeWeight('#range-count-from', '#range-count-to', '.range-count-start', '.range-count-end');
   initRangeWeight(
     '#range-weight-from',
